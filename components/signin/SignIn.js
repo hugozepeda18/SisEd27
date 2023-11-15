ea4e9519de387b690d8024824100f715eafc82c3
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2023-11-14 18:55:57 -0600

    Updating App

--- components/signin/SignIn.js
+++ components/signin/SignIn.js
@@ -19,7 +19,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit">
-        H Group 
+        A Group 
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
